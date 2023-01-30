f0829dea68fc339f89fc53149085f045c08586a2
RAGHAVENDRA <71554260+raghavk911@users.noreply.github.com>
2023-01-30 00:17:43 -0800

    Code has been alligned

--- force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
+++ force-app/main/default/aura/AuraCalculator/AuraCalculatorController.js
@@ -1,16 +1,15 @@
 ({
     press : function(component, event, helper) {
-        var buttonName = event.getSource().get("v.name");
-        console.log('buttonName=='+buttonName);
-        // Get the component event by using the
-        // name value from aura:registerEvent
-        var cmpEvent = component.getEvent("DisplayVaueEvent");
-        cmpEvent.setParams({"message" : buttonName });
-        cmpEvent.fire();
-        
-        var cmpEvent = component.getEvent("DisplayVaue");
-        cmpEvent.setParams({"messageDisplay" : buttonName });
-   
-        console.log('in press button event');
-    }		
+    var buttonName = event.getSource().get("v.name");
+    console.log('buttonName=='+buttonName);
+    // Get the component event by using the
+    // name value from aura:registerEvent
+    var cmpEvent = component.getEvent("DisplayVaueEvent");
+    cmpEvent.setParams({"message" : buttonName });
+    cmpEvent.fire();
+
+    var cmpEvent = component.getEvent("DisplayVaue");
+    cmpEvent.setParams({"messageDisplay" : buttonName });
+    console.log('in press button event');
+  }
 })
