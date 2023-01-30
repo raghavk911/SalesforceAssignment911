eb6de60d5a2334e862fe360fa32423416dc205c5
RAGHAVENDRA <71554260+raghavk911@users.noreply.github.com>
2023-01-30 04:59:59 -0800

    Revert "Assignment 8 aura"

--- force-app/main/default/aura/DisplayOutput/DisplayOutputController.js
+++
@@ -1,7 +0,0 @@
-({
-	handleApplicationEvent : function(component, event, helper) {
-		var message = event.getParam("messageDisplay");// event attribute name
-		// set the handler attributes based on event data
-		component.set("v.label", message);
-	}
-})
